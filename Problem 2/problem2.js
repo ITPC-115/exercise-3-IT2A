45f028a75c3b9df7be34a2b3a42f51752e7ee84a
John Paul M. Ra <114158516+JohnPaulMRa@users.noreply.github.com>
2023-10-04 09:59:36 +0800

    Update problem2.js

--- Problem 2/problem2.js
+++ Problem 2/problem2.js
@@ -4,5 +4,21 @@
     make sure that when the code is run in the browser it will log the
     properties of the object. -- Make sure to incorprate the keyword this in here.
 */
+const product = {
+    name: "Smartphone",
+    brand: "ExampleBrand",
+    price: 499.99,
+    color: "Black",
+
+
+    displayProperties: function () {
+        console.log(`Name: ${this.name}`);
+        console.log(`Brand: ${this.brand}`);
+        console.log(`Price: $${this.price}`);
+        console.log(`Color: ${this.color}`);
+    },
+};
+
+product.displayProperties();
 
 
