ffbcaf73210ef330ca36c8eb885aa6344ecc7af5
John Paul M. Ra <114158516+JohnPaulMRa@users.noreply.github.com>
2023-10-04 10:00:01 +0800

    Update problem3.js

--- Problem 3/problem3.js
+++ Problem 3/problem3.js
@@ -9,9 +9,27 @@
 */
 
 const student = new Object();
-const studentName =window.prompt('Enter Your Name');
+const studentName = window.prompt('Enter Your Name');
 const yearLevel = window.prompt('Enter Your Year Level (First, Second, Third, Fourth)');
-const age = window.prompt('Enter Age');
-const subjects = [{name: 'RE112', schedule: 'MWF'},{name:'SOSC143', schedule:'TTh'}];
+const age = parseInt(window.prompt('Enter Age')); 
+const subjects = [
+    { name: 'RE112', schedule: 'MWF' },
+    { name: 'SOSC143', schedule: 'TTh' },
+];
+
+
+student.name = studentName;
+student.yearLvl = yearLevel.toUpperCase(); 
+student.age = age;
+student.subjects = subjects;
+
+
+student.introduce = function () {
+    console.log(`My name is ${this.name}, ${this.yearLvl.toLowerCase()} year student, and ${this.age} years old.`);
+};
+
+
+console.log(student);
+student.introduce();
 
 
