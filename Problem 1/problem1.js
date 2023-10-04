69462744d031730032be6d7ab0a59063ae1228a8
Khryz Ervon L. Carreon <Carreon-Kh@ndmu.local>
2023-10-04 09:21:48 +0800

    number 1 palang

--- Problem 1/problem1.js
+++ Problem 1/problem1.js
@@ -8,3 +8,16 @@
     Lotty 22 Female
         -- make sure that name and sex are strings and age is a number when displayed
 */
+
+
+
+let Person1 = {
+    name: ['Khryz', 'Kyle', 'Karla', 'Tyshaun', 'Yhl', 'Rayveen', 'Crizle', 'Niel', 'Cess', 'Shandy'],
+    age: [20, 21, 23, 24, 22, 25, 23, 24, 26, 19],
+    sex: ['Male', 'Male', 'Female', 'Male', 'Male', 'Male', 'Female', 'Male', 'Female', 'Female']
+}
+
+for(let i =0; i < Person1.age.length; i++)
+{
+    console.log(Person1.name[i], Person1.age[i], Person1.sex[i]);
+}
