1915bac448cb5ecd729f4d5b2bfb74a7c6022846
John Paul M. Ra <114158516+JohnPaulMRa@users.noreply.github.com>
2023-10-04 10:09:30 +0800

    Update problem5.js

--- Problem 5/problem5.js
+++ Problem 5/problem5.js
@@ -7,3 +7,32 @@
     Constraints: The generated random number must be between 1 and 100 only.
 
 */
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+function playGuessingGame() {
+    let guess;
+    
+    do {
+       
+        guess = parseInt(prompt('Guess the number (between 1 and 100):'));
+
+        if (isNaN(guess)) {
+         
+            alert('Please enter a valid number.');
+        } else if (guess < 1 || guess > 100) {
+           
+            alert('Please enter a number between 1 and 100.');
+        } else if (guess < randomNumber) {
+            alert('Too low! Try again.');
+        } else if (guess > randomNumber) {
+            alert('Too high! Try again.');
+        }
+    } while (guess !== randomNumber);
+
+    console.log('Congratulations! You got it right.');
+}
+
+
+console.log('Generated Random Number:', randomNumber);
+
+playGuessingGame();
