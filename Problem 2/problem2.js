b5c700e1e551ef7631328c0b5e53ec559aa7389d
Rayveen John A. Escovidal <142953579+EscovidalRayveenJohn@users.noreply.github.com>
2023-10-11 07:59:57 +0800

    Update problem2.js

--- Problem 2/problem2.js
+++ Problem 2/problem2.js
@@ -4,5 +4,9 @@
     make sure that when the code is run in the browser it will log the
     properties of the object. -- Make sure to incorprate the keyword this in here.
 */
-
+const object = new Object();
+object.name = 'Vivo T1 5G';
+object.brand = 'Vivo';
+object.price = 17999;
+object.color = 'Blue';
 
