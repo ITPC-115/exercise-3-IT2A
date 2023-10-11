b99979f01984931ad7144db53109aa21bde0af82
Kim John A. Bautista <98477832+KimJohn911@users.noreply.github.com>
2023-10-11 13:32:51 +0800

    Revert "coding"

--- Problem 1/problem1.js
+++ Problem 1/problem1.js
@@ -8,19 +8,3 @@
     Lotty 22 Female
         -- make sure that name and sex are strings and age is a number when displayed
 */
-const hoomans = [
-    { name: "John", age: 24, sex: "Male" },
-    { name: "Marie", age: 27, sex: "Female" },
-    { name: "Lotty", age: 22, sex: "Female" },
-    { name: "Hitler", age: 56, sex: "Male"},
-    { name: "Jotaro", age: 40, sex: "Male"},
-    { name: "Princess", age: 19, sex: "Female"},
-    { name: "Oogway", age: 90, sex: "Male"},
-    { name: "Roe", age: 20, sex: "Male"},
-    { name: "Dryxe", age: 19, sex: "Male"},
-    { name: "Jesha", age: 20, sex: "Female"},
-];
-
-for (let peeps of hoomans) {
-    console.log(`${peeps.name} ${peeps.age} ${peeps.sex}`);
-}
--- Problem 2/problem2.js
+++ Problem 2/problem2.js
@@ -4,19 +4,5 @@
     make sure that when the code is run in the browser it will log the
     properties of the object. -- Make sure to incorprate the keyword this in here.
 */
-const product = {
-    name: "Softdrink",
-    brand: "coca-cola",
-    price: 20.00,
-    color: "red",
-    displayProperties: function() {
-        console.log(`Name: ${this.name}`);
-        console.log(`Brand: ${this.brand}`);
-        console.log(`Price: ${this.price}`);
-        console.log(`Color: ${this.color}`);
-    }
-};
 
-// Call the method to display the properties
-product.displayProperties();
 
