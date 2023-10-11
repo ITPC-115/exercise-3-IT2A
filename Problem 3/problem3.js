ffaf13feec596a57a3cb234b9d65dd09b14230ea
Rayveen John A. Escovidal <142953579+EscovidalRayveenJohn@users.noreply.github.com>
2023-10-11 08:57:10 +0800

    Update problem3.js

--- Problem 3/problem3.js
+++ Problem 3/problem3.js
@@ -15,3 +15,13 @@ const age = window.prompt('Enter Age');
 const subjects = [{name: 'RE112', schedule: 'MWF'},{name:'SOSC143', schedule:'TTh'}];
 
 
+student.name = studentName;
+student.yearLevel = yearLevel.toUpperCase();
+student.age = age;
+student.subjects = subjects;
+
+student.display = function(){
+    console.log("My name is " + this.name + ", " + this.yearLevel + " year student and " + this.age + " years old.")
+};
+console.log(student);
+student.display();
