616e4ad88f301226ea4c719982060e16e867b093
Tyshaun R. Fantilanan <Fantilanan-Ty@ndmu.local>
2023-10-11 08:12:57 +0800

    TYSHAUN PROBLEM 3

--- Problem 3/problem3.js
+++ Problem 3/problem3.js
@@ -9,9 +9,19 @@
 */
 
 const student = new Object();
-const studentName =window.prompt('Enter Your Name');
+const studentName = window.prompt('Enter Your Name');
 const yearLevel = window.prompt('Enter Your Year Level (First, Second, Third, Fourth)');
 const age = window.prompt('Enter Age');
 const subjects = [{name: 'RE112', schedule: 'MWF'},{name:'SOSC143', schedule:'TTh'}];
 
+student.name = studentName;
+student.yearLevel = yearLevel.toUpperCase();
+student.age = Number(age);
+student.subjects = subjects;
 
+
+student.display = function(){
+    console.log(`My name is ${this.name}, ${this.yearLevel} year student and ${this.age} years old.`);
+};
+console.log(student);
+student.display();
