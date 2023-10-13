762f398b5e63f7291b2779eadd34428d69302507
Facinabao, Shandy Liane <110117898+FacinabaoShandyLiane@users.noreply.github.com>
2023-10-13 09:27:30 +0800

    Update problem2.js

--- Problem 2/problem2.js
+++ Problem 2/problem2.js
@@ -4,5 +4,17 @@
     make sure that when the code is run in the browser it will log the
     properties of the object. -- Make sure to incorprate the keyword this in here.
 */
-
-
+let prop ={
+    name: 'iPhone 15',
+    brand:'Apple',
+    price: 62990.00,
+    color:'Purple',
+  
+    logeName: function() {
+    console.log(this.name);
+    console.log(this.brand);
+    console.log(this.price);
+    console.log(this.color);
+}
+}
+prop.logeName();
