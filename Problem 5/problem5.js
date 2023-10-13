d142c48fd3826bacf75f0a1515d961fc82246304
Joshua Ryett L. Initan <142952315+InitanJoshRyett@users.noreply.github.com>
2023-10-13 08:44:38 +0800

    NEW!

--- Problem 5/problem5.js
+++ Problem 5/problem5.js
@@ -7,3 +7,35 @@
     Constraints: The generated random number must be between 1 and 100 only.
 
 */
+
+function guessingGame() {
+  const min = 1;
+  const max = 100;
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  let attempts = 0;
+
+  console.log(`A random number between ${min} and ${max} has been generated. Try to guess it!`);
+
+  while (true) {
+    const userGuess = parseInt(prompt("Enter your guess:"));
+
+    if (isNaN(userGuess)) {
+      alert("Please enter a valid number.");
+    } else {
+      attempts++;
+
+      if (userGuess < min || userGuess > max) {
+        alert(`Please guess a number between ${min} and ${max}.`);
+      } else if (userGuess < randomNumber) {
+        alert("Try a higher number.");
+      } else if (userGuess > randomNumber) {
+        alert("Try a lower number.");
+      } else {
+        alert(`Congratulations! You got it right in ${attempts} attempts.`);
+        break;
+      }
+    }
+  }
+}
+
+guessingGame();
