70e046a24aa93c0bd8f3f96f774ec4c5a5e19f32
Facinabao, Shandy Liane <110117898+FacinabaoShandyLiane@users.noreply.github.com>
2023-10-13 09:26:42 +0800

    Update problem1.js

--- Problem 1/problem1.js
+++ Problem 1/problem1.js
@@ -8,3 +8,19 @@
     Lotty 22 Female
         -- make sure that name and sex are strings and age is a number when displayed
 */
+let person = [
+     {name: 'Shandy', age: 20, sex: 'Female'},
+     {name: 'Karla', age: 21, sex: 'Female'},
+     {name: 'Andrea', age: 21, sex: 'Female'},
+     {name: 'Khryz', age: 23, sex: 'Male'},
+     {name: 'Kyle', age: 22, sex: 'Male'},
+     {name: 'Tyshaun', age: 21, sex: 'Male'},
+     {name: 'Yhl', age: 21, sex: 'Male'},
+     {name: 'Rayveen', age: 23, sex: 'Male'},
+     {name: 'Crizle', age: 22, sex: 'Female'},
+     {name: 'Niel', age: 20, sex: 'Male'}];
+
+for(let i =0; i < 10; i++)
+{
+    console.log(person[i]);
+}
