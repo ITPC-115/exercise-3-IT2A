5195c389903f7fb77bed41581e062e0cb3b67f11
Joshua Ryett L. Initan <142952315+InitanJoshRyett@users.noreply.github.com>
2023-10-13 08:29:09 +0800

    NEW!

--- Problem 2/problem2.js
+++ Problem 2/problem2.js
@@ -5,4 +5,18 @@
     properties of the object. -- Make sure to incorprate the keyword this in here.
 */
 
-
+var product = {
+    name: "Laptop",
+    brand: "Lenovo",
+    price: 659.99,
+    color: "Matte Black",
+    displayProperties: function() {
+      console.log("Name: " + this.name);
+      console.log("Brand: " + this.brand);
+      console.log("Price: $" + this.price.toFixed(2)); // Format price as a decimal with 2 decimal places
+      console.log("Color: " + this.color);
+    }
+  };
+  
+  // Call the method to display the properties
+  product.displayProperties();
