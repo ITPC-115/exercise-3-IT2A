2ed70e7798065aee8ab577d74324097e3779c129
Karla Myles E. Gacias <142952623+GaciasKarlaMylesE@users.noreply.github.com>
2023-10-13 09:01:12 +0800

    Update problem1.js

--- Problem 1/problem1.js
+++ Problem 1/problem1.js
@@ -8,18 +8,19 @@
     Lotty 22 Female
         -- make sure that name and sex are strings and age is a number when displayed
 */
-const user = [
-    {
-        name : 'John', age: 24, sex: 'Male',
-    },
-    {
-        name : 'Marie', age: 27, sex: 'Female',
-    },
-    {
-        name : 'Lotty', age: 22, sex: 'Female',
-    },
-]
-for (let i = 0; i < user.length; i++){
-    const person = user[i];
-    console.log(person.name + ' ' + person.age + ' ' + person.sex);
-}
+let people = [
+    {name: "John", age: 24, sex: "Male"},
+    {name: "Karla", age: 27, sex: "Female"},
+    {name: "Shandy", age: 22, sex: "Female"},
+    {name: "Kyle", age: 30, sex: "Male"},
+    {name: "Ervon", age: 19, sex: "Male"},
+    {name: "David", age: 45, sex: "Male"},
+    {name: "Andrea", age: 33, sex: "Female"},
+    {name: "Carl", age: 28, sex: "Male"},
+    {name: "Ryett", age: 31, sex: "Male"},
+    {name: "Arvee", age: 26, sex: "Male"}
+  ];
+  
+  for (let k = 0; k <10; k++) {
+    console.log(people[k]);
+  }
