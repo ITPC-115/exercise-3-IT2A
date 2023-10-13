a2d4cbd7c2e2176009723815181ae7bc41590c8f
John Paul M. Ra <114158516+JohnPaulMRa@users.noreply.github.com>
2023-10-13 08:02:01 +0800

    Update problem1.js

--- Problem 1/problem1.js
+++ Problem 1/problem1.js
@@ -12,6 +12,13 @@ const people = [
     { name: "John", age: 24, sex: "Male" },
     { name: "Marie", age: 27, sex: "Female" },
     { name: "Lotty", age: 22, sex: "Female" },
+    { name: "Aldos", age: 56, sex: "Male" },
+    { name: "Loyla", age: 19, sex: "Female" },
+    { name: "Zilong", age: 23, sex: "Male" },
+    { name: "Miya", age: 19, sex: "Female" },
+    { name: "Liljohn", age: 20, sex: "Male" },
+    { name: "Khalifa", age: 21, sex: "Female" },
+    { name: "Lexy", age: 28, sex: "Female" },
    
 ];
 
