737406ab6570b97d62ec44e1571280ae14a15434
Student <student@ndmu.local>
2023-10-16 08:55:53 +0800

    Answers

--- Problem 1/problem1.js
+++ Problem 1/problem1.js
@@ -1,10 +1,14 @@
-/*
-    Define an array that contains 10 objects which has the properties
-    name, age, and sex. Iterate throughout the array and display each of their name, age, and sex (one object per line in console).
-
-    Example:
-    John 24 Male
-    Marie 27 Female
-    Lotty 22 Female
-        -- make sure that name and sex are strings and age is a number when displayed
-*/
+ const people = [{name: 'Khryz', age: 20, sex: 'Male'},
+               {name: 'Kyle', age: 20, sex: 'Male'},
+               {name: 'Karla', age: 20, sex: 'Female'},
+               {name: 'Tyshaun', age: 20, sex: 'Male'},
+               {name: 'Yhl', age: 20, sex: 'Male'},
+               {name: 'Rayveen', age: 20, sex: 'Male'},
+               {name: 'Crizle', age: 20, sex: 'Female'},
+               {name: 'Niel', age: 20, sex: 'Male'},
+               {name: 'Princess', age: 20, sex: 'Female'},
+               {name: 'Shandy', age: 20, sex: 'Female'},];
+for(let i =0; i < 10; i++)
+{
+    console.log(people[i]);
+}
--- Problem 5/problem5.js
+++ Problem 5/problem5.js
@@ -7,3 +7,17 @@
     Constraints: The generated random number must be between 1 and 100 only.
 
 */
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(`Generated random number: ${randomNumber}`);
+
+let guessNumber = parseInt(prompt("Guess a number between 1 and 100:"));
+
+while (guessNumber !== randomNumber) {
+    if (guessNumber < randomNumber) {
+        guessNumber = parseInt(prompt("Too low! Guess again:"));
+    } else {
+        guessNumber = parseInt(prompt("Too high! Guess again:"));
+    }
+}
+console.log("Congratulations you got it right!");
