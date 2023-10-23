f6dedbb664d1bac097181bdbd9eda6c28fd5b03e
Ianskie <142952548+AngelesJanIan@users.noreply.github.com>
2023-10-23 21:24:02 +0800

    Update problem2.js

--- Problem 2/problem2.js
+++ Problem 2/problem2.js
@@ -4,5 +4,18 @@
     make sure that when the code is run in the browser it will log the
     properties of the object. -- Make sure to incorprate the keyword this in here.
 */
+const phone = {
+    name: 'iPhone 15', // Changed name to iPhone 15
+    brand: 'Apple',
+    price: 126000, // Changed price to 126,000
+    color: 'White',
+    logProperties: function(){
+        console.log(this.name);
+        console.log(this.brand);
+        console.log(this.price);
+        console.log(this.color);
+    }
+}
+phone.logProperties();
 
 
