08c7768c2eb55e961ca8397ce0a10e77ee19549b
Ianskie <142952548+AngelesJanIan@users.noreply.github.com>
2023-10-23 21:34:28 +0800

    Update problem3.js

--- Problem 3/problem3.js
+++ Problem 3/problem3.js
@@ -14,4 +14,8 @@ const yearLevel = window.prompt('Enter Your Year Level (First, Second, Third, Fo
 const age = window.prompt('Enter Age');
 const subjects = [{name: 'RE112', schedule: 'MWF'},{name:'SOSC143', schedule:'TTh'}];
 
-
+student.showcase = function(){
+    console.log("My name is " + student.name + ", " + this.yearLvl.toLowerCase() +  " year student and " + this.age +  " years old.");
+}
+console.log(student);
+student.showcase(); 
